8fbf37ba6975cc611d57218598d4230c8bff6f8a
mai ha an <maihaan24@gmail.com>
2018-09-14 13:15:33 +0700

    Upload code
    Upload code

---
+++ PrepairData/PrepairData/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PrepairData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btRead = new System.Windows.Forms.Button();
+            this.tbEvents = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(321, 12);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 0;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(13, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(302, 20);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(13, 39);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(75, 23);
+            this.btRead.TabIndex = 2;
+            this.btRead.Text = "Read";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // tbEvents
+            // 
+            this.tbEvents.Location = new System.Drawing.Point(13, 68);
+            this.tbEvents.Multiline = true;
+            this.tbEvents.Name = "tbEvents";
+            this.tbEvents.Size = new System.Drawing.Size(383, 325);
+            this.tbEvents.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 405);
+            this.Controls.Add(this.tbEvents);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.TextBox tbEvents;
+    }
+}
+
---
+++ PrepairData/PrepairData/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PrepairData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if(od.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = od.FileName;
+            }
+               
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            String[] lines = System.IO.File.ReadAllLines(tbPath.Text);
+            List<String> events = new List<string>();
+            foreach(String line in lines)
+            {
+                // Lay tat ca cac su kien
+                if(line.StartsWith("[2]") || line.StartsWith("[3]"))
+                {
+                    String data = line.Substring(4);
+                    foreach(String ev in data.Split(','))
+                    {
+                        if (!events.Contains(ev.Trim()))
+                            events.Add(ev.Trim());
+                    }
+                }
+            }
+
+            // Cap nhat cac su kien vao CSDL
+            DataTable tb = ReadData("Select Max(Ma) as MaLN from tbSuKien");
+            int maLN = 0;
+            if (tb != null && tb.Rows.Count > 0)
+                int.TryParse(tb.Rows[0]["MaLN"].ToString(), out maLN);
+
+            foreach(String ev in events)
+            {
+                if(!String.IsNullOrEmpty(ev))
+                {
+                    maLN++;
+                    String query = "Insert into tbSuKien(Ten,Ma) Values(N'" + ev + "'," + maLN.ToString() + ")";
+                    WriteData(query);
+                }                
+            }
+
+            // Cap nhat cac luat vao CSDL
+            for(int i = 0; i < lines.Length; i++)
+            {
+                if(lines[i].StartsWith("[1]"))
+                {
+                    String tenLuat = lines[i].Substring(4);
+                    String veTrai = lines[i + 1].Substring(4);
+                    String vePhai = lines[i + 2].Substring(4);
+
+                    // Tach ve phai neu ve phai co nhieu hon 1 su kien
+                    foreach(String eRight in vePhai.Split(','))
+                    {
+                        if (!String.IsNullOrEmpty(eRight))
+                        {
+                            // Lay ma lon nhat trong csdl luat
+                            DataTable tb1 = ReadData("Select Max(Ma) as MaLN from tbLuat");
+                            int maLuatLN = 0;
+                            if (tb1 != null && tb1.Rows.Count > 0)
+                                int.TryParse(tb1.Rows[0]["MaLN"].ToString(), out maLuatLN);
+
+                            // Lay ma cua su kien o ve phai
+                            DataTable tbERight = ReadData("Select Ma From tbSuKien Where Ten=N'" + eRight.Trim() + "'");
+                            int maERight = int.Parse(tbERight.Rows[0]["Ma"].ToString());
+
+                            // tach ra thanh nhieu dong nhung van la 1 luat
+                            foreach (String eLeft in veTrai.Split(','))
+                            {
+                                // Lay ma cua su kien o ve trai
+                                DataTable tbELeft = ReadData("Select Ma From tbSuKien Where Ten=N'" + eLeft.Trim() + "'");
+                                int maELeft = int.Parse(tbELeft.Rows[0]["Ma"].ToString());
+
+                                String queryLuat = "Insert into tbLuat(Ten, Ma, VeTrai, VePhai) Values(N'"
+                                    + tenLuat + "', " + (maLuatLN + 1).ToString() + "," + maELeft.ToString() + "," + maERight.ToString() + ")";
+                                WriteData(queryLuat);
+                            }
+                        }
+                    }
+                    
+
+                }
+            }
+            MessageBox.Show("OK");
+        }
+
+        private int WriteData(String query)
+        {
+            String connectionString = "Data Source=.\\MSSQL; Initial Catalog=DSS; user id=admin; Password=@n123456;";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                int dem = cmd.ExecuteNonQuery();
+                con.Close();
+                return dem;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        private DataTable ReadData(String query)
+        {
+            String connectionString = "Data Source=.\\MSSQL; Initial Catalog=DSS; user id=admin; Password=@n123456;";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable tb = new DataTable();
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                tb.Load(dr, LoadOption.OverwriteChanges);
+                con.Close();
+                return tb;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
